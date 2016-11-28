526a609b4edf880975b9f687b89a03285fb9630d
Dawvid <dssd1001@gmail.com>
2016-11-27 23:25:28 -0800

    update tests

--- test/controllers/apply_pages_controller_test.rb
+++ test/controllers/apply_pages_controller_test.rb
@@ -2,12 +2,12 @@ require 'test_helper'
 
 class ApplyPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get member" do
-    get apply_pages_member_url
+    get apply_member_path
     assert_response :success
   end
 
   test "should get client" do
-    get apply_pages_client_url
+    get apply_client_path
     assert_response :success
   end
 
--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,28 +1,28 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get pages_home_url
+  test "should get root" do
+    get root_path
     assert_response :success
   end
 
   test "should get about" do
-    get pages_about_url
+    get about_path
     assert_response :success
   end
 
   test "should get portfolio" do
-    get pages_portfolio_url
+    get portfolio_path
     assert_response :success
   end
 
   test "should get connections" do
-    get pages_connections_url
+    get connections_path
     assert_response :success
   end
 
   test "should get updates" do
-    get pages_updates_url
+    get updates_path
     assert_response :success
   end
 
