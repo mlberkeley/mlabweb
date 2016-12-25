e776f31817db46f7d2548eb848b506be6d2b6516
Dawvid <dssd1001@gmail.com>
2016-12-24 17:47:29 -0800

    make members controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
   root 'pages#home'
+
+  # Static pages routes
   get '/about', to: 'pages#about'
   get '/portfolio', to: 'pages#portfolio'
   get '/connections', to: 'pages#connections'
   get '/updates', to: 'pages#updates'
   get '/legal', to: 'pages#legal'
 
+  # Member routes
+  get '/create', to: 'members#new'
+
+  # Application routes
   get '/apply', to: 'apply_pages#index'
   get '/apply/member', to: 'apply_pages#member'
   get '/apply/client', to: 'apply_pages#client'
