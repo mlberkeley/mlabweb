959cd2930538b1d46259add269d7dce7b4fa56f4
Dawvid <dssd1001@gmail.com>
2016-12-26 12:57:47 -0800

    update login

--- app/helpers/members_helper.rb
+++ app/helpers/members_helper.rb
@@ -3,7 +3,7 @@ module MembersHelper
   # Returns the Picture for the given member
   def picture_of(member)
     pic_path = "team/#{member.name.downcase}.jpg"
-    File.exist?(Rails.root.join('app/assets/images/', pic_path)) ? path = pic_path : path = "team/unpictured.jpg"
+    path = File.exist?(Rails.root.join('app/assets/images/', pic_path)) ? pic_path : "team/unpictured.jpg"
     image_tag(path, alt: "Member Picture", class: "img-circle team-img")
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161225015938) do
+ActiveRecord::Schema.define(version: 20161226203711) do
 
   create_table "members", force: :cascade do |t|
     t.string   "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20161225015938) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "remember_digest"
     t.index ["email"], name: "index_members_on_email", unique: true
   end
 
