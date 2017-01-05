6a01de057a01ce8a99cd718c8520a2fd025d3d20
Dawvid <dssd1001@gmail.com>
2017-01-04 21:22:25 -0800

    Mailer test v7

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
     :user_name            => Rails.application.secrets.gmail_username,
     :password             => Rails.application.secrets.gmail_password,
     :authentication       => :plain,
-    :domain               => 'heroku.com',
+    :domain               => 'mlabweb.herokuapp.com',
     :enable_starttls_auto => true
   }
 
