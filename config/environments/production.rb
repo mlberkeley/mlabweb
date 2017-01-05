4fa1b0fb17f5375a0abc9ce6d64131fa4369ad96
Dawvid <dssd1001@gmail.com>
2017-01-04 21:17:58 -0800

    Mailer test v6

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   host = 'mlabweb.herokuapp.com'
@@ -70,7 +70,7 @@ Rails.application.configure do
     :user_name            => Rails.application.secrets.gmail_username,
     :password             => Rails.application.secrets.gmail_password,
     :authentication       => :plain,
-    :domain               => 'mlabweb.herokuapp.com',
+    :domain               => 'heroku.com',
     :enable_starttls_auto => true
   }
 
