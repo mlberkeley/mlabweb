ea671657d1650b3257aac5f809698c24d60ebe3d
Dawvid <dssd1001@gmail.com>
2017-01-24 15:27:43 -0800

    uniqueness of attendance roll creation

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,4 @@
 class Event < ApplicationRecord
   has_and_belongs_to_many :members, -> { distinct }
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
 end
