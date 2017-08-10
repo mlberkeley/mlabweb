ea567443f58f6c6b473d026db29875963b60b6c8
Dawvid <dssd1001@gmail.com>
2017-08-10 15:55:22 -0700

    Update config to connect to MySQL server on 'pandemic.OCF.Berkeley.EDU'

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,8 +63,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  # host = 'ml.berkeley.edu'
-  host = 'mlabwebv2.herokuapp.com'
+  host = 'ml.berkeley.edu'
   config.action_mailer.default_url_options = { host: host }
   config.action_mailer.smtp_settings = {
     :address              => 'smtp.gmail.com',
@@ -72,7 +71,7 @@ Rails.application.configure do
     :user_name            => ENV["GMAIL_USERNAME"],
     :password             => ENV["GMAIL_PASSWORD"],
     :authentication       => :plain,
-    :domain               => 'mlabwebv2.herokuapp.com',
+    :domain               => 'ml.berkeley.edu',
     :enable_starttls_auto => true
   }
 
