a5b12f6926c8dcaaf7dcfde185fb489c0c26eb2a
Dawvid <dssd1001@gmail.com>
2018-01-20 18:21:09 -0800

    fix JS precompilation issue on production

--- app/assets/javascripts/controller_specific/members.js
+++ app/assets/javascripts/controller_specific/members.js
@@ -1,11 +1,16 @@
 //= require jquery.Jcrop
 
-jQuery(() => new PictureCropper());
+var PictureCropper,
+  bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
 
-class PictureCropper {
-  constructor() {
-    this.update = this.update.bind(this);
-    this.updatePreview = this.updatePreview.bind(this);
+jQuery(function() {
+  return new PictureCropper();
+});
+
+PictureCropper = (function() {
+  function PictureCropper() {
+    this.updatePreview = bind(this.updatePreview, this);
+    this.update = bind(this.update, this);
     $('#cropbox').Jcrop({
       aspectRatio: 1,
       setSelect: [0, 0, 600, 600],
@@ -14,20 +19,23 @@ class PictureCropper {
     });
   }
 
-  update(coords) {
+  PictureCropper.prototype.update = function(coords) {
     $('#member_crop_x').val(coords.x);
     $('#member_crop_y').val(coords.y);
     $('#member_crop_w').val(coords.w);
     $('#member_crop_h').val(coords.h);
     return this.updatePreview(coords);
-  }
+  };
 
-  updatePreview(coords) {
+  PictureCropper.prototype.updatePreview = function(coords) {
     return $('#preview').css({
-        width: Math.round((100/coords.w) * $('#cropbox').width()) + 'px',
-        height: Math.round((100/coords.h) * $('#cropbox').height()) + 'px',
-        marginLeft: `-${Math.round((100/coords.w) * coords.x)}px`,
-        marginTop: `-${Math.round((100/coords.h) * coords.y)}px`
+      width: Math.round(100 / coords.w * $('#cropbox').width()) + 'px',
+      height: Math.round(100 / coords.h * $('#cropbox').height()) + 'px',
+      marginLeft: '-' + Math.round(100 / coords.w * coords.x) + 'px',
+      marginTop: '-' + Math.round(100 / coords.h * coords.y) + 'px'
     });
-  }
-}
+  };
+
+  return PictureCropper;
+
+})();
--- app/assets/javascripts/controller_specific/projects.js
+++ app/assets/javascripts/controller_specific/projects.js
@@ -14,13 +14,17 @@ $(document).on('turbolinks:click', function (event) {
 
 });
 
+var BgCropper,
+  bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
 
-jQuery(() => new BgCropper());
+jQuery(function() {
+  return new BgCropper();
+});
 
-class BgCropper {
-  constructor() {
-    this.update = this.update.bind(this);
-    this.updatePreview = this.updatePreview.bind(this);
+BgCropper = (function() {
+  function BgCropper() {
+    this.updatePreview = bind(this.updatePreview, this);
+    this.update = bind(this.update, this);
     $('#cropbox').Jcrop({
       aspectRatio: 4.25,
       setSelect: [0, 0, 425, 100],
@@ -29,20 +33,23 @@ class BgCropper {
     });
   }
 
-  update(coords) {
+  BgCropper.prototype.update = function(coords) {
     $('#project_crop_x').val(coords.x);
-    $('#project_crop_y').val(coords.y);
-    $('#project_crop_w').val(coords.w);
-    $('#project_crop_h').val(coords.h);
+    $('#project_crop_x').val(coords.y);
+    $('#project_crop_x').val(coords.w);
+    $('#project_crop_x').val(coords.h);
     return this.updatePreview(coords);
-  }
+  };
 
-  updatePreview(coords) {
+  BgCropper.prototype.updatePreview = function(coords) {
     return $('#preview').css({
-        width: Math.round((100/coords.w) * $('#cropbox').width()) + 'px',
-        height: Math.round((100/coords.h) * $('#cropbox').height()) + 'px',
-        marginLeft: `-${Math.round((100/coords.w) * coords.x)}px`,
-        marginTop: `-${Math.round((100/coords.h) * coords.y)}px`
+      width: Math.round(100 / coords.w * $('#cropbox').width()) + 'px',
+      height: Math.round(100 / coords.h * $('#cropbox').height()) + 'px',
+      marginLeft: '-' + Math.round(100 / coords.w * coords.x) + 'px',
+      marginTop: '-' + Math.round(100 / coords.h * coords.y) + 'px'
     });
-  }
-}
+  };
+
+  return BgCropper;
+
+})();
